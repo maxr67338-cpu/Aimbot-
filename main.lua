0c58465536bd55b3df42a9d227bb692886518184
maxr67338-cpu <maxr67338@gmail.com>
2026-07-12 22:37:21 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -11,7 +11,7 @@ end
 local aimbotEnabled = false -- Startet auf false, bis Key korrekt ist
 local espEnabled = false    -- Startet auf false, bis Key korrekt ist
 local keyVerified = false   -- Status für das Key-System
-local correctKey = "Thanksfor300"
+local correctKey = "Thanksfor300hits"
 
 -- AIMBOT EINSTELLUNGEN
 local maxFOV = 250         -- Sichtfeld-Radius für den Aimbot
